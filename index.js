16bd5b5145ff782ead84290e9ff2da3c445a11de
Charles Kettell <charleskettell@hotmail.co.uk>
2019-09-17 18:33:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on post ${PORT}`));
